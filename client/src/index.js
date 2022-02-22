fa343503f1c11f565c8b843966a5e84989799f0c
Prabhjyot <knowprabhjyot@gmail.com>
2022-02-22 12:15:19 -0800

    added color theme to project

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "5BAF61",
+    },
+    secondary: {
+      main: "#F15A22",
+    },
+    accent: {
+      main: "#FDC350",
+    },
+    warning: {
+      main: red[500],
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
