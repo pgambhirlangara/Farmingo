1785da13f9e66e435b213d3c63f9b366dc4bd075
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-02-24 10:53:45 -0800

    Server side of farmer signup

--- server/routes/Farmer.js
+++ server/routes/Farmer.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const { registerFarmer, loginFarmer, getFarmers, updateFarmer, deleteFarmer, getFarmerById } = require('../controller/Farmer');
+
+router.use(logger);
+
+router.post('/register', registerFarmer);
+
+router.post('/login', loginFarmer);
+
+router.get('/', getFarmers);
+
+router.put('/:id', updateFarmer);
+
+router.delete('/:id', deleteFarmer);
+
+router.get('/:id', getFarmerById);
+
+function logger(req, res, next) {
+    console.log('Runnning your requests.....');
+    next();
+}
+
 
 
 module.exports = router;
