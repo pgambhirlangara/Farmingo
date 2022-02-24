d6def96b06f965d8ee50c0d0c150f0448e62b33e
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-02-24 01:38:09 -0800

    Create dropdown.jsx
    Basic dropdown component

---
+++ client/src/components/utils/dropdown.jsx
@@ -0,0 +1,7 @@
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const options = [];
+const defaultOption = options[0];
+<Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
+
