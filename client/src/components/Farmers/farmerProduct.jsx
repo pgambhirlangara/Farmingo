7e8690b223eed8ddafeefec54bc3f6c095b83c77
Prabhjyot <knowprabhjyot@gmail.com>
2022-02-27 01:36:47 -0800

    added border to main content

--- client/src/components/Farmers/farmerProduct.jsx
+++ client/src/components/Farmers/farmerProduct.jsx
@@ -10,8 +10,15 @@ const FarmerProductPage = () => {
         banner: {
             width: "100%"
         },
-        farmNameSection: {
+        farmProductContainer: {
             background: "#EEF6EE",
+        },
+        farmProductSubContainer: {
+            background: "white",
+            border: `1px solid ${theme.palette.primary.main}`,
+            borderRadius: "20px"
+        },
+        farmNameSection: {
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
@@ -58,23 +65,27 @@ const FarmerProductPage = () => {
     return (
         <div>
             <img className={classes.banner} src="../../../assets/farmer_home_banner.jpg" alt="Home" />
-            <section className={classes.farmNameSection}>
-                <h1 className={classes.farmName}>Hi! Cloverdale Market Farm</h1>
-                <p className={classes.farmAddress}>5688 168 St, Surrey, BC V3W 4C8</p>
-            </section>
-            <section className={classes.farmPostSection}>
-                <h3>Post something new!</h3>
-                <Button type="button" variant="contained" color="secondary" endIcon={<AddIcon />}>Create new post</Button>
-                <h3>View and Edit your products</h3>
-                <h4>Here is the list of your products on sale, click in to the product to manage the list and update it.</h4>
-            </section>
-            <section className={classes.farmProducts}>
-                {
-                    farmProducts.map((item) => {
-                        return <FarmerProductItem name={item.name} price={item.price} image={item.image} />
-                    })
-                }
-            </section>
+            <div className={classes.farmProductContainer}>
+                <section className={classes.farmNameSection}>
+                    <h1 className={classes.farmName}>Hi! Cloverdale Market Farm</h1>
+                    <p className={classes.farmAddress}>5688 168 St, Surrey, BC V3W 4C8</p>
+                </section>
+               <div className={classes.farmProductSubContainer}>
+               <section className={classes.farmPostSection}>
+                    <h3>Post something new!</h3>
+                    <Button type="button" variant="contained" color="secondary" endIcon={<AddIcon />}>Create new post</Button>
+                    <h3>View and Edit your products</h3>
+                    <h4>Here is the list of your products on sale, click in to the product to manage the list and update it.</h4>
+                </section>
+                <section className={classes.farmProducts}>
+                    {
+                        farmProducts.map((item) => {
+                            return <FarmerProductItem name={item.name} price={item.price} image={item.image} />
+                        })
+                    }
+                </section>
+               </div>
+            </div>
         </div>
     )
 }
