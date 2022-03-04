567506e117169a8734ab574882f2952e04597dc5
nassneg <asnegur00@mylangara.ca>
2022-03-03 18:20:01 -0800

    create FarmerOrderHistory page

---
+++ client/src/components/Farmers/farmerOrderHistory.jsx
@@ -0,0 +1,19 @@
+import { Button, Paper } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Box } from "@mui/system";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+const FarmerOrderHistory = (props) => {
+  const useStyles = makeStyles((theme) => ({
+
+  }));
+
+  const classes = useStyles();
+
+  return (
+
+  );
+};
+
+export default FarmerOrderHistory;
