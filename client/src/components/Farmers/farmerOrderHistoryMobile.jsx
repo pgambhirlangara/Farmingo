adb1b8b40071900ef3f46d30dfc6ba8e4e57c96c
nassneg <asnegur00@mylangara.ca>
2022-03-07 18:06:36 -0800

    Mobile menu for the order history
    Layout implemented

---
+++ client/src/components/Farmers/farmerOrderHistoryMobile.jsx
@@ -0,0 +1,66 @@
+import { Button, Paper } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+
+const FarmerOrderHistoryMobile = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    farmOrderHistory: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      backgroundColor: "#EEF6EE",
+    },
+    farmTotalSection: {
+      width: "100%",
+      textAlign: "center",
+      backgroundColor: "#5BAF61",
+      marginBottom: "10px",
+      color: "#FFFFFF",
+    },
+    farmButtonsSection: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      width: "80%",
+      gap: "10px",
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.farmOrderHistory}>
+      <section className={classes.farmHeaderSection}>
+        <h1 className={classes.farmHeader}>Order History</h1>
+      </section>
+
+      <section className={classes.farmTotalSection}>
+        <p className={classes.farmTotal}>Total:</p>
+        <p className={classes.farmDate}>Date:</p>
+      </section>
+
+      <Box className={classes.farmButtonsSection}>
+        <Button type="button" variant="contained" color="secondary">
+          New orders
+        </Button>
+
+        <Button type="button" variant="contained" color="secondary">
+          In progress orders
+        </Button>
+
+        <Button type="button" variant="contained" color="secondary">
+          Completed orders
+        </Button>
+
+        <Button type="button" variant="contained" color="secondary">
+          Problem orders
+        </Button>
+      </Box>
+    </div>
+  );
+};
+
+export default FarmerOrderHistoryMobile;
