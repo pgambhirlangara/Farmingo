5b2b716543dec8720b1f245f22dad09c3139c916
nassneg <asnegur00@mylangara.ca>
2022-03-10 21:24:41 -0800

    made the route accessible in backend

--- server/app.js
+++ server/app.js
@@ -1,37 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const customerRoutes = require('./routes/Customer');
-const farmerRoutes = require('./routes/Farmer');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan')
-require('dotenv').config();
-
+const customerRoutes = require("./routes/Customer");
+const farmerRoutes = require("./routes/Farmer");
+const orderRoutes = require("./routes/Order");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Database connected...');
-})
+mongoose.connection.on("connected", () => {
+  console.log("Database connected...");
+});
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-
-app.get('/', (request, response) => {
-    console.log('Requested home page');
-    response.send('HOME PAGE');
-})
+app.get("/", (request, response) => {
+  console.log("Requested home page");
+  response.send("HOME PAGE");
+});
 
 // middleware
-app.use('/customer', customerRoutes);
-app.use('/farmer', farmerRoutes);
+app.use("/customer", customerRoutes);
+app.use("/farmer", farmerRoutes);
+app.use("/order", orderRoutes);
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
-
